87cd5a3da360c0d33c487d5e7aa7b50db11d1c49
waldoayo <waldoayo@53e7e1fc-f952-11de-9a59-372c7e1f5a1b>
2010-05-15 05:38:56 +0000

    adding policy tester

---
+++ tools/policy-tool.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "libfenc.h"
+#include "libfenc_group_params.h"
+#include "libfenc_ABE_common.h"			
+#include "libfenc_LSSS.h"				
+#include "policy_lang.h"
+
+#define MAX_STR 512
+
+/* tool to test policy parsing */
+int main(int argc, char *argv[]) {
+	
+	/* make sure we only have one argument */
+	if(argc != 2) {
+		printf("Usage: %s [ policy-string ]\n", argv[0]);
+		exit(1);
+	}
+
+	FENC_ERROR result;
+	char *policy = argv[1];
+	char policy_str[MAX_STR];
+	size_t policy_str_len = MAX_STR;
+	/* allocate policy structure */
+	fenc_attribute_policy *parsed_policy = (fenc_attribute_policy *) malloc(sizeof(fenc_attribute_policy));
+	if(parsed_policy == NULL) {
+		printf("parsed_policy is NULL!");
+	}
+	memset(parsed_policy, 0, sizeof(fenc_attribute_policy)); 
+	
+	/* convert policy string into policy structure */
+	fenc_policy_from_string(parsed_policy, policy);
+
+	/* serialize policy struct to buffer */	
+	memset(policy_str, 0, MAX_STR);
+	result = fenc_attribute_policy_to_string(parsed_policy->root, policy_str, policy_str_len);
+
+	/* see if they are equivalent? */
+	printf("Output:\t'%s'\n", policy_str);
+	return 0;
+}
+
