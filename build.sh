e0922abf24509a6948acf480e5cc9d1547794d4c
waldoayo <waldoayo@53e7e1fc-f952-11de-9a59-372c7e1f5a1b>
2010-07-07 21:54:35 +0000

    minor updates

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# (Temporary script)
+# Description: this script will build all the libfenc shared object and prototype tools that use the library.
+# Author: Joseph Ayo Akinyele
+
+cmd=$1
+
+if [ $# -eq 0 ]; then
+   echo "$0: [ option ]"
+   echo "\toptions: help, all, src, tools"
+   exit 1
+fi
+
+if [ $cmd == "all" ]; then
+   echo "Building src and tools..."
+   $(cd src; ./configure CC="gcc -arch i386" CXX="g++ -arch i386")
+   make -C src/
+   $(cd tools; ./configure CC="gcc -arch i386" CXX="g++ -arch i386")
+   make -C tools/
+elif [ $cmd == "src" ]; then
+   echo "Building src..." 
+elif [ $cmd == "tools" ]; then
+   echo "Building tools..." 
+elif [ $cmd == "clean" ]; then
+   make -C src/ clean
+   make -C tools/ clean
+else
+   echo "$0: [ option ]"
+   echo "\toptions: help, all, src, tools"
+fi
+
+exit 0
